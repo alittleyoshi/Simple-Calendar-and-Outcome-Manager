bd9b8c0e8c9184eac0e93c6d370b78a52500de26
alittleyoshi <892370173@qq.com>
2024-09-22 16:56:41 +0800

    Update SCOM.cpp
    Nothing new but I change my editor

--- SCOM.cpp
+++ SCOM.cpp
@@ -4,6 +4,13 @@ using namespace std;
 	Simple Calender and Output Manager aka. SCOM
 	by Alittlezhi.
 */
+int mainUI() {
+	/*
+	´òÓ¡Ö÷Ò³
+	*/
+	cout << "Simple Calender and Output Manager";
+
+}
 int main() {
 
 	return 0;
