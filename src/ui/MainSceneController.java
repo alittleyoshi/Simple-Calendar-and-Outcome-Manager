88ff8b823ac455ccad10a92e388a299c50cda44b
SYpiece <105969639+SYpiece@users.noreply.github.com>
2024-10-30 14:44:24 +0800

    update 0000002

--- src/ui/MainSceneController.java
+++ src/ui/MainSceneController.java
@@ -1,4 +1,29 @@
 package ui;
 
-public class MainSceneController {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.control.ToggleGroup;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MainSceneController implements Initializable {
+    @FXML
+    private ToggleGroup _tabButtonGroup;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        _tabButtonGroup.selectedToggleProperty().addListener((observable, oldValue, newValue) -> {
+//            System.out.println("oldValue: " + oldValue + " oldUserData: " + oldValue.getUserData() + " newValue: " + newValue + " newUserData: " + newValue.getUserData());
+            if (newValue == null) {
+                oldValue.setSelected(true);
+            } else {
+                if (oldValue != null) {
+                    ((Parent) oldValue.getUserData()).setVisible(false);
+                }
+                ((Parent) newValue.getUserData()).setVisible(true);
+            }
+        });
+    }
 }
