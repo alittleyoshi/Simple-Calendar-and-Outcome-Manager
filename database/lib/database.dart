14d2db844dac9baa4394d5b14b55ca119fd83631
Kibi <kibiandkimi@outlook.com>
2025-01-27 22:55:00 +0800

    minor changed in database

--- database/lib/database.dart
+++ database/lib/database.dart
@@ -19,27 +19,30 @@ final _bindings = DatabaseBindings(Platform.isLinux ?
     DynamicLibrary.open('database.dll'));
 
 final initDatabaseC = _bindings.Dart_init;
+final preGetTaskListC = _bindings.Dart_get_list_pre;
+final getTaskListC =  _bindings.Dart_get_list;
+final preGetTaskC = _bindings.Dart_get_task_pre;
+final getTaskC =  _bindings.Dart_get_task;
+final addTaskListC =  _bindings.Dart_create_tasklist;
+final addTaskC =  _bindings.Dart_create_task;
+final updateTaskC =  _bindings.Dart_update_task;
+final deleteTaskC =  _bindings.Dart_delete_task;
+final deleteTaskListC =  _bindings.Dart_delete_tasklist;
 
-final queryTaskListId = _bindings.Dart_query_tasklist_id;
-
+@Deprecated('No need to query num after init')
 final queryTaskListNum =  _bindings.Dart_query_tasklist_num;
 
+@Deprecated('Use Dart_get_list')
+final queryTaskListId = _bindings.Dart_query_tasklist_id;
+
+@Deprecated('Use Dart_get_list')
 final queryTaskListName =  _bindings.Dart_query_tasklist_name;
 
+@Deprecated('No need to query task num after init.')
 final queryTaskNum =  _bindings.Dart_query_task_num;
 
-final getTaskC =  _bindings.Dart_get_task;
-
-final addTaskList =  _bindings.Dart_create_tasklist;
-
-final addTaskC =  _bindings.Dart_create_task;
-
+@Deprecated('Use Dart_update_task')
 final updateStatTaskC =  _bindings.Dart_update_task_stat;
 
-final updateTaskC =  _bindings.Dart_update_task;
-
+@Deprecated('Use Dart_update_task')
 final moveTaskC =  _bindings.Dart_move_task;
-
-final deleteTaskC =  _bindings.Dart_delete_task;
-
-final deleteTaskListC =  _bindings.Dart_delete_tasklist;
